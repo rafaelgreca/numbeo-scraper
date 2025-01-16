5484cad86c7eeabb6ea115c2f2a3c5b617797fcf
Rafael Greca <rgvieira97@gmail.com>
2025-01-16 01:23:21 -0300

    adding examples with multiple years

--- src/schema/input.py
+++ src/schema/input.py
@@ -20,7 +20,35 @@ class Input(BaseModel):
         "traffic",
     ]
     mode: str = Literal["country", "city"]
-    years: Union[int, List[Union[int, str]]] = Literal[2023, 2024, 2025]
+    years: Union[int, List[Union[int, str]]] = Literal[
+        2012,
+        "2012-mid",
+        2013,
+        "2013-mid",
+        2014,
+        "2014-mid",
+        2015,
+        "2015-mid",
+        2016,
+        "2016-mid",
+        2017,
+        "2017-mid",
+        2018,
+        "2018-mid",
+        2019,
+        "2019-mid",
+        2020,
+        "2020-mid",
+        2021,
+        "2021-mid",
+        2022,
+        "2022-mid",
+        2023,
+        "2023-mid",
+        2024,
+        "2024-mid",
+        2025,
+    ]
     regions: Optional[Union[str, List[str], None]] = Literal[
         "Africa", "America", "Asia", "Europe", "Oceania"
     ]
