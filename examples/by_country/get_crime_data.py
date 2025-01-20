98bc6142d91382d506b5fd2de694e7b4275a2ede
Rafael Greca <rgvieira97@gmail.com>
2025-01-19 22:52:59 -0300

    refactoring examples folder

---
+++ examples/by_country/get_crime_data.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from src.core.utils import read_yaml_credentials_file
+from src.schema.input import Input
+from src.core.scraper import NumbeoScraper
+
+
+if __name__ == "__main__":
+    # reading the YAML file
+    config = Input(
+        **read_yaml_credentials_file(
+            file_path=Path.joinpath(
+                Path(__file__).resolve().parents[1],
+                "configs",
+            ),
+            file_name="crime_country.yaml",
+        )
+    )
+
+    scraper = NumbeoScraper(
+        config=config,
+    )
+    dataframes = scraper.scrap()  # will return a list of tuples
+
+    # since we only have one category, the list length is 1
+    dataframe_name, data = dataframes[0]  # the name is used to identify the data
+
+    print(f"\nDataframe '{dataframe_name}' has a shape of {data.shape}.")
+    print(f"The first five rows of the dataset:\n{data.head(5)}\n")
